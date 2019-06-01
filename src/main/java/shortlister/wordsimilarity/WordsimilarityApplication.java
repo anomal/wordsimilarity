59b8264dcc598b0b238ab1dca1d9a449140e09e3
Sophiah Ho <sophiah.ho@gmail.com>
2019-06-01 15:34:12 -0400

    init

---
+++ src/main/java/shortlister/wordsimilarity/WordsimilarityApplication.java
@@ -0,0 +1,13 @@
+package shortlister.wordsimilarity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsimilarityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsimilarityApplication.class, args);
+	}
+
+}
