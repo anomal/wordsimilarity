10802952e77f6d0191940f3805aebc3c92123bf6
Sophiah Ho <sophiah.ho@gmail.com>
2019-06-03 14:48:52 -0400

    add generated-sources to classpath

--- src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
+++ src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
@@ -1,16 +1,18 @@
 package shortlister.service.word.similarity.rest.controller;
 
+import io.swagger.client.model.WordSimilarityRequest;
+import io.swagger.client.model.WordSimilarityResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WordSimilarityRestController {
-/*
+
     @RequestMapping("/v1/resumes")
     public WordSimilarityResponse postResumes(@RequestBody WordSimilarityRequest request) {
         WordSimilarityResponse response = new WordSimilarityResponse();
         return response;
     }
-    */
+
 }
