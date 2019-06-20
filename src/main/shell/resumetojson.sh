e57e51d317e553847b3203f79b1ce200ab97e83d
Sophiah Ho <sophiah.ho@gmail.com>
2019-06-20 17:16:36 -0400

    add shell script to gen req resumes

---
+++ src/main/shell/resumetojson.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $# -lt  1 ]]
+then
+	echo "Usage: $0 resumePath" >&2
+	exit 1
+fi
+
+path=$1
+echo "["
+
+i=0
+for r in `find $path -type f`
+do
+	if [[ $i -gt 0 ]]
+	then
+		echo ','
+	fi
+ 
+	text=`cat $r`
+	echo '    {'
+	echo '        "id" : "'$i'",'
+	echo '        "text" : "'$text'"'
+	echo '    }'
+	((i++))
+done
+echo "]"
+
