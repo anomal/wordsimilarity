43c8156659b79c1de16b4b30825b03961f34d239
Sophiah Ho <sophiah.ho@gmail.com>
2019-06-29 14:27:01 -0400

    implement 403 Forbidden

--- src/main/java/shortlister/service/word/similarity/exception/UnauthorizedException.java
+++
@@ -1,8 +0,0 @@
-package shortlister.service.word.similarity.exception;
-
-public class UnauthorizedException extends Exception {
-
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
--- src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
+++ src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
@@ -5,14 +5,15 @@ import io.swagger.client.model.WordSimilarityResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import shortlister.service.word.similarity.WordSimilarityService;
-import shortlister.service.word.similarity.exception.UnauthorizedException;
 
-import java.io.IOException;
 import java.util.UUID;
 
 @RestController
@@ -35,20 +36,22 @@ public class WordSimilarityRestController {
 
     @RequestMapping("/v1/resumes")
     public WordSimilarityResponse postResumes(@RequestHeader("authorization") String authorization,
-                                              @RequestBody WordSimilarityRequest request) throws UnauthorizedException {
+                                              @RequestBody WordSimilarityRequest request) {
         WordSimilarityResponse response = null;
         try {
             if (authorization.length() < BEARER_LENGTH) {
-                throw new UnauthorizedException("Unauthorized");
+                throw new AccessDeniedException("Unauthorized");
             } else {
                 String token = authorization.substring(BEARER_LENGTH).trim();
                 if (!token.equals(DEV_TOKEN)) {
-                    throw new UnauthorizedException("Unauthorized");
+                    throw new AccessDeniedException("Unauthorized");
                 }
             }
 
             response = service.analyze(request.getResumes(), request.getWordAttraction());
             return response;
+        } catch (AccessDeniedException e) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage(), e);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw e;
