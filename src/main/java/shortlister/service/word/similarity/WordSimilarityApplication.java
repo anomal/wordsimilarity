5b68aba132f4658b7c596ab63164831917acfdf5
Sophiah Ho <sophiah.ho@gmail.com>
2019-06-29 13:52:32 -0400

    use dev token for authorization

--- src/main/java/shortlister/service/word/similarity/WordSimilarityApplication.java
+++ src/main/java/shortlister/service/word/similarity/WordSimilarityApplication.java
@@ -1,13 +1,19 @@
 package shortlister.service.word.similarity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import shortlister.service.word.similarity.rest.controller.WordSimilarityRestController;
 
 @SpringBootApplication
 public class WordSimilarityApplication {
 
+	private final static Logger log = LoggerFactory.getLogger(WordSimilarityApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(WordSimilarityApplication.class, args);
+		log.info("DEV_TOKEN: {}", WordSimilarityRestController.DEV_TOKEN);
 	}
 
 }
--- src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
+++ src/main/java/shortlister/service/word/similarity/rest/controller/WordSimilarityRestController.java
@@ -6,29 +6,39 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shortlister.service.word.similarity.WordSimilarityService;
+import shortlister.service.word.similarity.exception.UnauthorizedException;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 public class WordSimilarityRestController {
 
-    private static Logger log = LoggerFactory.getLogger(WordSimilarityRestController.class);
+    public final static String DEV_TOKEN = UUID.randomUUID().toString().replace("-","");
+    private final static int BEARER_LENGTH = "Bearer ".length();
+    private final static Logger log = LoggerFactory.getLogger(WordSimilarityRestController.class);
 
     @Autowired
     private WordSimilarityService service;
 
     @RequestMapping("/v1/resumes")
-    public WordSimilarityResponse postResumes(@RequestBody WordSimilarityRequest request) {
+    public WordSimilarityResponse postResumes(@RequestHeader("authorization") String authorization,
+                                              @RequestBody WordSimilarityRequest request) throws UnauthorizedException {
         WordSimilarityResponse response = null;
         try {
+            if (authorization.length() < BEARER_LENGTH
+                    || !authorization.substring(BEARER_LENGTH ).trim().equals(DEV_TOKEN)) {
+                throw new UnauthorizedException("request token is " + authorization.substring(BEARER_LENGTH ).trim());
+            }
             response = service.analyze(request.getResumes(), request.getWordAttraction());
-            return  response;
-        } catch (RuntimeException e) {
+            return response;
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
-            return null;
+            throw e;
         }
     }
 
